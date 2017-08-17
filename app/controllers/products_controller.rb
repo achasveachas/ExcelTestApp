f54d30b8f27d0cb67c2c7192fcee7bfb70c0ba8c
Yechiel Kalmenson <contact@yechiel.me>
2017-08-17 11:58:54 -0400

    Updated the productscontroller to respond to xlsx

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,10 @@
 class ProductsController < ApplicationController
     def index
         @products = Product.all
+
+        respond_to do |format|
+            format.html
+            format.xlsx
+        end
     end
 end
